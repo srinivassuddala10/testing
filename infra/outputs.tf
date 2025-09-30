cfc94a0a0c7614c6674e74ae8b7cd2ac74ffd0f3
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-09-30 10:23:55 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,18 +1,7 @@
 output "bucket_name" {
-  value = module.s3_bucket.s3_bucket_id
+  value = aws_s3_bucket.my_bucket.bucket
 }
 
-output "bucket_website_endpoint" {
-  value = module.s3_bucket.s3_bucket_website_endpoint
+output "ec2_public_ip" {
+  value = aws_instance.my_ec2.public_ip
 }
-
-output "bucket_website_domain" {
-  value = module.s3_bucket.s3_bucket_website_domain
-}
-
-output "rds" {
-  value       = aws_db_instance.postgres_instance
-  description = "The endpoint of the PostgreSQL RDS instance"
-  sensitive   = true
-}
-
