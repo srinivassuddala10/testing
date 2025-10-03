9ba4a3f9ad987a2aa59f9b509ef074df0c608d0e
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-03 17:23:29 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,14 +1,14 @@
-# S3 bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-# EC2 instance
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 in us-east-1
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "MyEC2"
-  }
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
