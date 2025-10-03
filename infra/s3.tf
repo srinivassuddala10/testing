c81db28a66095543b45654d4b55aff8dfd90cdf5
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-03 18:22:14 +0530

    Delete infra/s3.tf

--- infra/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "frontend_bucket" {
-  bucket = "${var.project_name}-frontend-${random_id.bucket_id.hex}"
-  acl    = "private"
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
-}
