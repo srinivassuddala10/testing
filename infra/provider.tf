b67377f48a4b91beeb64fd09c6277ca2153a16b8
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-03 17:42:03 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
