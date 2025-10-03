2dcb55f8618188cd4cbde7f80e6e06865b972feb
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-03 18:48:46 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,10 +3,15 @@ resource "aws_s3_bucket" "frontend_bucket" {
 }
 
 # ---------------------------
-# Backend API EC2 instance
-resource "aws_instance" "backend_ec2" {
+resource "aws_instance" "web" {
   ami           = "ami-0abcdef1234567890"
   instance_type = "t3.micro"
+  subnet_id     = aws_subnet.main.id  # reference your subnet
+  security_groups = [aws_security_group.ec2_sg.name]  # reference a valid SG
+
+  tags = {
+    Name = "WebServer"
+  }
 }
 
 # --------------------------
@@ -24,5 +29,25 @@ resource "aws_db_instance" "postgres" {
   skip_final_snapshot = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   subnet_group_name      = aws_db_subnet_group.default.name
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow Postgres access"
+  vpc_id      = aws_vpc.main.id  # make sure you have a VPC defined
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # adjust for security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
