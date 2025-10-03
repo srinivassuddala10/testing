875985bba1c05a12a9e1f0a2143eb31cc5731a6e
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-03 18:21:11 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,18 +1,11 @@
-resource "aws_db_subnet_group" "db_subnets" {
-  name       = "${var.project_name}-subnet-group"
-  subnet_ids = [aws_subnet.main.id]
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
 }
 
-resource "aws_db_instance" "postgres" {
-  identifier              = "${var.project_name}-db"
-  engine                  = "postgres"
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
-  name                    = "mydb"
-  username                = var.db_username
-  password                = var.db_password
-  db_subnet_group_name    = aws_db_subnet_group.db_subnets.name
-  vpc_security_group_ids  = [aws_security_group.ec2_sg.id]
-  skip_final_snapshot     = true
-  publicly_accessible     = false
+output "s3_bucket_name" {
+  value = aws_s3_bucket.frontend_bucket.id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.endpoint
 }
