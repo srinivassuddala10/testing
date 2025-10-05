c2312188622cc5ed3f957740d0a6f22ad3a96e92
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-05 22:31:53 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  required_version = ">= 1.4.0"
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 # ---------------------------
 # VPC
 # ---------------------------
